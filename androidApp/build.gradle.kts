d27bf21c8019112c097069ae289e2ee07cdb05a2
Rafa Ortega <rafao1989@hotmail.com>
2021-03-11 19:30:00 +0100

    add compose to android UI

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -9,6 +9,12 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.2")
+
+    // compose
+    implementation("androidx.compose.ui:ui:1.0.0-beta02")
+    implementation("androidx.compose.ui:ui-tooling:1.0.0-beta01")
+    implementation("androidx.compose.material:material:1.0.0-beta01")
+    implementation("androidx.activity:activity-compose:1.3.0-alpha04")
 }
 
 android {
@@ -24,5 +30,22 @@ android {
         getByName("release") {
             isMinifyEnabled = false
         }
+    }
+    buildFeatures {
+        // Enables Jetpack Compose for this module
+        compose = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.0.0-beta01"
     }
 }
--- androidApp/src/main/java/com/keller/yourpet/androidApp/MainActivity.kt
+++ androidApp/src/main/java/com/keller/yourpet/androidApp/MainActivity.kt
@@ -1,40 +1,62 @@
 package com.keller.yourpet.androidApp
 
 import android.os.Bundle
-import android.widget.TextView
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.tooling.preview.Preview
+import com.keller.yourpet.androidApp.ui.YourPetUITheme
 import com.keller.yourpet.shared.api.PetsApiClient
 import com.keller.yourpet.shared.database.DatabaseDriverFactory
 import com.keller.yourpet.shared.database.DatabaseModule
 import com.keller.yourpet.shared.database.PetsDataBaseHelper
+import com.keller.yourpet.shared.model.Pet
 import com.keller.yourpet.shared.repository.PetsRepository
 import com.keller.yourpet.shared.usecase.GetPetsUseCase
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val tv: TextView = findViewById(R.id.text_view)
 
         val databaseDriverFactory = DatabaseDriverFactory(this)
         val database = DatabaseModule().createDataBase(databaseDriverFactory.createDriver())
 
-        // TODO don't use global scope
-        GlobalScope.launch {
-            GetPetsUseCase(PetsRepository(PetsDataBaseHelper(database), PetsApiClient())).execute()
-                .map { it.toString() }
-                .collect {
-                    withContext(Dispatchers.Main) {
-                        tv.text = it
+        setContent {
+            YourPetUITheme {
+                val scope = rememberCoroutineScope()
+                val petsList = mutableStateOf(emptyList<Pet>())
+                scope.launch {
+                    GetPetsUseCase(
+                        PetsRepository(
+                            PetsDataBaseHelper(database),
+                            PetsApiClient()
+                        )
+                    ).execute().collect {
+                        petsList.value = it
                     }
                 }
+                Surface(color = MaterialTheme.colors.background) { PetsNames(pets = petsList.value) }
+            }
         }
+    }
+}
+
+@Composable
+fun PetsNames(pets: List<Pet>) {
+    Text(text = pets.toString())
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    YourPetUITheme {
+        PetsNames(listOf(Pet("charlie")))
     }
 }
