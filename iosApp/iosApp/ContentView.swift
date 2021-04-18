4ede6f1c2503dfe26e0e08aa234cfd23e924fe97
Rafa Ortega <rafao1989@hotmail.com>
2021-04-18 17:37:32 +0200

    add pets title as navigation title

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
                             PetRow(pet: pet)
                         }
                     }
+                    .customNavigationTitle(with: "Pets")
                 }
             case .error(let message):
                 Text(message)
---
+++ iosApp/iosApp/NavigationBarTitle.swift
@@ -0,0 +1,41 @@
+//
+//  NavigationBarTitle.swift
+//  iosApp
+//
+//  Created by Rafael Ortega on 18/04/2021.
+//  Copyright © 2021 orgName. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+@available(iOS 14, *)
+struct NavigationTitleViewModifier: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        content
+            .navigationTitle(text)
+    }
+}
+struct NavigationBarTitleViewModifier: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        content
+            .navigationBarTitle(text)
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func customNavigationTitle(with text: String) -> some View {
+        if #available(iOS 14, *) {
+            self
+                .modifier(NavigationTitleViewModifier(text: text))
+        }
+        else {
+            self.modifier(NavigationBarTitleViewModifier(text: text))
+        }
+    }
+}
