6d5c8f2eb827a0091b5dd4f627813deef05fc994
Rafa Ortega <rafao1989@hotmail.com>
2021-08-31 22:25:26 +0200

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
 
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
-        classpath("com.android.tools.build:gradle:7.1.0-alpha08")
+        classpath("com.android.tools.build:gradle:7.1.0-alpha10")
         classpath("org.jetbrains.kotlin:kotlin-serialization:${Versions.kotlin}")
         classpath("com.google.dagger:hilt-android-gradle-plugin:${Versions.hilt}")
 
