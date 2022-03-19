d4edb364a931a538093aabaf18ea1a1e254e4e1b
Rafa Ortega <rafao1989@hotmail.com>
2022-03-19 17:34:40 +0100

    add entry point to show case in side menu for debug builds

--- androidApp/src/main/java/com/keller/yourpet/androidApp/petslist/view/menu/SideMenu.kt
+++ androidApp/src/main/java/com/keller/yourpet/androidApp/petslist/view/menu/SideMenu.kt
@@ -2,22 +2,39 @@ package com.keller.yourpet.androidApp.petslist.view.menu
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import com.airbnb.android.showkase.models.Showkase
+import com.keller.yourpet.androidApp.BuildConfig
+import com.keller.yourpet.androidApp.ui.getBrowserIntent
 import com.keller.yourpet.shared.model.Gender
 
 @Composable
 fun SideMenu(onGenderSelected: (Gender, Boolean) -> Unit) {
+    val context = LocalContext.current
+
     Column(modifier = Modifier.padding(8.dp)) {
         Text("Filters")
         for (gender in Gender.values())
             LabelledCheckbox(gender, onGenderSelected)
+
+        Spacer(modifier = Modifier.weight(1.0f))
+        if (BuildConfig.DEBUG) {
+            Button(onClick = {
+                context.startActivity(Showkase.getBrowserIntent(context))
+            }) {
+                Text("Showkase browser")
+            }
+        }
     }
 }
 
