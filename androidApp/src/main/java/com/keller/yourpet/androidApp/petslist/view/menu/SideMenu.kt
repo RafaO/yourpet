334a32676ef7595345e2b5d246fdd43aba1a1dec
Rafa Ortega <rafao1989@hotmail.com>
2022-06-11 22:06:45 +0200

    add menu item composable

--- androidApp/src/main/java/com/keller/yourpet/androidApp/petslist/view/menu/SideMenu.kt
+++ androidApp/src/main/java/com/keller/yourpet/androidApp/petslist/view/menu/SideMenu.kt
@@ -1,25 +1,39 @@
 package com.keller.yourpet.androidApp.petslist.view.menu
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.airbnb.android.showkase.models.Showkase
 import com.keller.yourpet.androidApp.BuildConfig
+import com.keller.yourpet.androidApp.R
 import com.keller.yourpet.androidApp.ui.UIGroups
 import com.keller.yourpet.androidApp.ui.YourPetUITheme
 import com.keller.yourpet.androidApp.ui.getBrowserIntent
 import com.keller.yourpet.shared.model.Gender
+
+@Composable
+fun MenuItem(text: String, onClick: () -> Unit) {
+    Text(
+        text,
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick)
+            .height(dimensionResource(R.dimen.height_clickable))
+            .padding(start = dimensionResource(R.dimen.text_padding))
+            .wrapContentHeight(Alignment.CenterVertically)
+    )
+}
 
 @Composable
 fun SideMenu(onGenderSelected: (Gender, Boolean) -> Unit) {
@@ -29,6 +43,8 @@ fun SideMenu(onGenderSelected: (Gender, Boolean) -> Unit) {
         Text("Filters")
         for (gender in Gender.values())
             LabelledCheckbox(gender, onGenderSelected)
+
+        MenuItem("Settings") {}
 
         Spacer(modifier = Modifier.weight(1.0f))
         if (BuildConfig.DEBUG) {
