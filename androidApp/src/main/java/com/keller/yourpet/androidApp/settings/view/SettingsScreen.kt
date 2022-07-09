6a319d2453161aba6bdf356a0757866a9212fc58
Rafa Ortega <rafao1989@hotmail.com>
2022-07-09 18:13:24 +0200

    improve setting screens UI

--- androidApp/src/main/java/com/keller/yourpet/androidApp/settings/view/SettingsScreen.kt
+++ androidApp/src/main/java/com/keller/yourpet/androidApp/settings/view/SettingsScreen.kt
@@ -1,18 +1,19 @@
 package com.keller.yourpet.androidApp.settings.view
 
 import androidx.activity.ComponentActivity
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -22,9 +23,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.window.PopupProperties
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.keller.yourpet.androidApp.R
 import com.keller.yourpet.androidApp.main.viewmodel.MainViewModel
 import com.keller.yourpet.androidApp.main.viewmodel.ThemeColor
 import com.keller.yourpet.androidApp.ui.UIGroups
@@ -36,17 +39,19 @@ fun SettingsScreen() {
     var themeMenuExpanded by remember { mutableStateOf(false) }
 
     Column {
-        Row {
+        Row(
+            modifier = Modifier
+                .padding(dimensionResource(R.dimen.text_padding))
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             Text("Theme")
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .wrapContentSize(Alignment.TopEnd)
-            ) {
-                Row(modifier = Modifier) {
+            Box {
+                Row(verticalAlignment = Alignment.CenterVertically) {
                     Text(text = uiState.themeColor.toString())
                     IconButton(onClick = { themeMenuExpanded = true }) {
-                        Icon(Icons.Default.MoreVert, contentDescription = "Theme")
+                        Icon(Icons.Default.Edit, contentDescription = "Theme")
                     }
                 }
                 DropdownMenu(
