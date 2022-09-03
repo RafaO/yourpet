3aa951d7751673f2470e022e72e44c84bd700141
Rafa Ortega <rafao1989@hotmail.com>
2022-09-03 18:59:59 +0200

    [iOS] add picker menu to settings for theme selection

--- iosApp/iosApp/Settings/SettingsView.swift
+++ iosApp/iosApp/Settings/SettingsView.swift
@@ -10,8 +10,19 @@ import Foundation
 import SwiftUI
 
 struct SettingsView: View {
+    @State private var selectedTheme = 0
     
     var body: some View {
-        return Text("Settings")
+        return VStack(spacing: 10) {
+                Text("Settings")
+                HStack() {
+                    Text("Theme")
+                    Spacer()
+                    Picker(selection: $selectedTheme, label: Text("Theme")) {
+                        Text("light")
+                        Text("dark")
+                    }.pickerStyle(MenuPickerStyle())
+                }
+            }
     }
 }
