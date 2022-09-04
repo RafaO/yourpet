e6fb08d771eee8034b9e487174b1e20494c1587f
Rafa Ortega <rafao1989@hotmail.com>
2022-09-04 14:11:17 +0200

    [iOS] remove unused variable in HomeViewModel

--- iosApp/iosApp/MainView/MainView.swift
+++ iosApp/iosApp/MainView/MainView.swift
@@ -4,8 +4,6 @@ import shared
 struct MainView: View {
     @ObservedObject var viewModel: MainViewModel
     
-    private let filter = Filter(genders: [.Female(), .Male()])
-    
     var body: some View {
         HomeView(homeViewModel: HomeViewModel())
             .environment(\.colorScheme, viewModel.state.colorScheme)
