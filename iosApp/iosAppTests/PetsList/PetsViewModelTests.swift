74bdac43c91e5ea5136bbf710896e330089749d5
Rafa Ortega <rafao1989@hotmail.com>
2022-10-17 22:59:16 +0200

    [iOS] use parametrized tests on in pets view model test

--- iosApp/iosAppTests/PetsList/PetsViewModelTests.swift
+++ iosApp/iosAppTests/PetsList/PetsViewModelTests.swift
@@ -27,6 +27,60 @@ class GetPetsUseCaseMock: GetPetsUseCaseInterface {
     }
 }
 
+class PetsViewModelStateTests: XCTestCase {
+    private var useCaseResult: BaseFlowableUseCaseResult<NSArray>? = nil
+    private var expectedState: PetsScreenState? = nil
+    
+    private let genders  = [
+        Gender_.female: true,
+        Gender_.male: true,
+    ]
+
+    override open class var defaultTestSuite: XCTestSuite {
+        let petMock = Pet(name: "", imageUrl: "", gender: Gender_.female)
+        let errorMessage = "pets not found"
+        let testSuite = XCTestSuite(name: NSStringFromClass(self))
+        addTestsWithArray(
+            BaseFlowableUseCaseResultSuccess<NSArray>(result: [petMock]),
+            expected: PetsScreenState.content([petMock]),
+            toTestSuite: testSuite
+        )
+        addTestsWithArray(
+            BaseFlowableUseCaseResultFailure(error: KotlinException(message: errorMessage)),
+            expected: PetsScreenState.error(errorMessage),
+            toTestSuite: testSuite
+        )
+        return testSuite
+    }
+
+    private class func addTestsWithArray(_ result: BaseFlowableUseCaseResult<NSArray>,
+                                         expected: PetsScreenState,
+                                         toTestSuite testSuite: XCTestSuite) {
+        testInvocations.forEach { invocation in
+            let testCase = PetsViewModelStateTests(invocation: invocation)
+            testCase.useCaseResult = result
+            testCase.expectedState = expected
+            testSuite.addTest(testCase)
+        }
+    }
+    
+    func testState() throws {
+        // given
+        let useCaseMock = GetPetsUseCaseMock()
+        
+        useCaseMock.useCaseResult = (FlowUtilsKt.createCFlow(initialValue: useCaseResult) as! CFlow<BaseFlowableUseCaseResult<AnyObject>>)
+        let subject = PetsViewModel(getPetsUseCase: useCaseMock)
+
+        // when
+        subject.viewCreated(genders: genders)
+
+        // then
+        let statePublisher = subject.$state.collectNext(1)
+        let state = try awaitPublisher(statePublisher).first
+        XCTAssertTrue(state == expectedState)
+    }
+}
+
 class PetsViewModelTests: XCTestCase {
     
     private let genders  = [
@@ -44,25 +98,6 @@ class PetsViewModelTests: XCTestCase {
         
         // then
         XCTAssertTrue(useCaseMock.executed)
-    }
-    
-    func testWhenUseCaseSucceedsStateIsContent() throws {
-        // given
-        let useCaseMock = GetPetsUseCaseMock()
-        let petMock = Pet(name: "", imageUrl: "", gender: Gender_.female)
-        
-        let useCaseResult = BaseFlowableUseCaseResultSuccess<NSArray>(result: [petMock])
-        useCaseMock.useCaseResult = (FlowUtilsKt.createCFlow(initialValue: useCaseResult) as! CFlow<BaseFlowableUseCaseResult<AnyObject>>)
-        let subject = PetsViewModel(getPetsUseCase: useCaseMock)
-
-        // when
-        subject.viewCreated(genders: genders)
-
-        // then
-        let statePublisher = subject.$state.collectNext(1)
-        let state = try awaitPublisher(statePublisher).first
-        let expected = PetsScreenState.content([petMock])
-        XCTAssertTrue(state == expected)
     }
     
     func testInitialStateIsLoading() throws {
