aa5ef9e3cdf25d9aa8be87aab2fb7a405cffddce
Rafa Ortega <rafao1989@hotmail.com>
2022-10-18 19:50:27 +0200

    [iOS] use for loop to do parametrized testing on pets view model

--- iosApp/iosAppTests/PetsList/PetsViewModelTests.swift
+++ iosApp/iosAppTests/PetsList/PetsViewModelTests.swift
@@ -12,56 +12,39 @@ import Foundation
 import shared
 
 class PetsViewModelStateTests: XCTestCase {
-    private var useCaseResult: BaseFlowableUseCaseResult<NSArray>? = nil
-    private var expectedState: PetsScreenState? = nil
-    
     private let genders  = [
         Gender_.female: true,
         Gender_.male: true,
     ]
 
-    override open class var defaultTestSuite: XCTestSuite {
+    func testState() throws {
+
         let petMock = Pet(name: "", imageUrl: "", gender: Gender_.female)
         let errorMessage = "pets not found"
-        let testSuite = XCTestSuite(name: NSStringFromClass(self))
-        addTestsWithArray(
-            BaseFlowableUseCaseResultSuccess<NSArray>(result: [petMock]),
-            expected: PetsScreenState.content([petMock]),
-            toTestSuite: testSuite
-        )
-        addTestsWithArray(
-            BaseFlowableUseCaseResultFailure(error: KotlinException(message: errorMessage)),
-            expected: PetsScreenState.error(errorMessage),
-            toTestSuite: testSuite
-        )
-        return testSuite
-    }
 
-    private class func addTestsWithArray(_ result: BaseFlowableUseCaseResult<NSArray>,
-                                         expected: PetsScreenState,
-                                         toTestSuite testSuite: XCTestSuite) {
-        testInvocations.forEach { invocation in
-            let testCase = PetsViewModelStateTests(invocation: invocation)
-            testCase.useCaseResult = result
-            testCase.expectedState = expected
-            testSuite.addTest(testCase)
+        let testValues = [
+            BaseFlowableUseCaseResultSuccess<NSArray>(result: [petMock]):
+            PetsScreenState.content([petMock]),
+
+            BaseFlowableUseCaseResultFailure<AnyObject>(error: KotlinException(message: errorMessage)):
+            PetsScreenState.error(errorMessage),
+        ]
+
+        for (useCaseResult, expectedState) in testValues {
+            // given
+            let useCaseMock = GetPetsUseCaseMock()
+
+            useCaseMock.useCaseResult = (FlowUtilsKt.createCFlow(initialValue: useCaseResult) as! CFlow<BaseFlowableUseCaseResult<AnyObject>>)
+            let subject = PetsViewModel(getPetsUseCase: useCaseMock)
+
+            // when
+            subject.viewCreated(genders: genders)
+
+            // then
+            let statePublisher = subject.$state.collectNext(1)
+            let state = try awaitPublisher(statePublisher).first
+            XCTAssertTrue(state == expectedState)
         }
-    }
-    
-    func testState() throws {
-        // given
-        let useCaseMock = GetPetsUseCaseMock()
-        
-        useCaseMock.useCaseResult = (FlowUtilsKt.createCFlow(initialValue: useCaseResult) as! CFlow<BaseFlowableUseCaseResult<AnyObject>>)
-        let subject = PetsViewModel(getPetsUseCase: useCaseMock)
-
-        // when
-        subject.viewCreated(genders: genders)
-
-        // then
-        let statePublisher = subject.$state.collectNext(1)
-        let state = try awaitPublisher(statePublisher).first
-        XCTAssertTrue(state == expectedState)
     }
 }
 
