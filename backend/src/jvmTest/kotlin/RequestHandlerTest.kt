aa9d89785506572053587588a5b03d1439b9d413
Rafa Ortega <rafao1989@hotmail.com>
2022-10-29 23:55:55 +0200

    [be] add test to request handler for exception in db

--- backend/src/jvmTest/kotlin/RequestHandlerTest.kt
+++ backend/src/jvmTest/kotlin/RequestHandlerTest.kt
@@ -12,6 +12,7 @@ class RequestHandlerTest {
 
     // TODO move this to a common mock factory module
     private fun mockPetsList() = listOf(Pet("Charlie", "", Gender.Female))
+    private val filter = Filter(mutableSetOf(Gender.Female, Gender.Male))
 
     @Test
     fun `when pets are requested, returns the pets in the db`() = runTest {
@@ -22,9 +23,23 @@ class RequestHandlerTest {
         val subject = RequestHandler(dbHelper)
 
         // when
-        val result = subject.getPets(Filter(mutableSetOf(Gender.Female, Gender.Male)))
+        val result = subject.getPets(filter)
 
         // then
         assertEquals(petsInDB, result)
+    }
+
+    @Test
+    fun `when db throws exception, returns empty list`() = runTest {
+        // given
+        val dbHelper = mockk<DBHelper>()
+        coEvery { dbHelper.getPets(any()) } throws RuntimeException()
+        val subject = RequestHandler(dbHelper)
+
+        // when
+        val result = subject.getPets(filter)
+
+        // then
+        assertEquals(emptyList<Pet>(), result)
     }
 }
