a3d075fb8c448206d8a11cc62669fe4bcbcbc2de
Rafa Ortega <rafao1989@hotmail.com>
2023-02-05 12:55:52 +0100

    [shared] reduce line length in mock factory

--- shared/src/testResources/kotlin/com.keller.yourpet.shared.mock/MockDataFactory.kt
+++ shared/src/testResources/kotlin/com.keller.yourpet.shared.mock/MockDataFactory.kt
@@ -4,7 +4,12 @@ import com.keller.yourpet.shared.model.Gender
 import com.keller.yourpet.shared.model.Pet
 import kotlin.random.Random
 
-fun mockPet(id: Long = Random.nextLong()) = Pet(id.toString(), "Fatality", "https://picsum.photos/id/237/200/150", Gender.Female)
+fun mockPet(id: Long = Random.nextLong()) = Pet(
+    id.toString(),
+    "Fatality",
+    "https://picsum.photos/id/237/200/150",
+    Gender.Female
+)
 
 fun mockPetsList() = listOf(
     Pet("1", "Fatality", "https://picsum.photos/id/237/200/150", Gender.Female),
