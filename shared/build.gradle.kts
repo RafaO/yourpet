ff2b0bffbc58d79174f554a6d951034ed973c579
Rafa Ortega <rafao1989@hotmail.com>
2023-02-05 12:15:01 +0100

    [ios] use new way of building for ios

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,5 +1,4 @@
 import Versions.coroutinesVersion
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 apply(from = "../tools/detekt.gradle")
 
@@ -128,19 +127,3 @@ sqldelight {
         packageName = "com.keller.yourpet.shared.data"
     }
 }
-
-val packForXcode by tasks.creating(Sync::class) {
-    group = "build"
-    val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
-    val sdkName = System.getenv("SDK_NAME") ?: "iphonesimulator"
-    val targetName = "ios" + if (sdkName.startsWith("iphoneos")) "Arm64" else "X64"
-    val framework =
-        kotlin.targets.getByName<KotlinNativeTarget>(targetName).binaries.getFramework(mode)
-    inputs.property("mode", mode)
-    dependsOn(framework.linkTask)
-    val targetDir = File(buildDir, "xcode-frameworks")
-    from({ framework.outputDirectory })
-    into(targetDir)
-}
-
-tasks.getByName("build").dependsOn(packForXcode)
