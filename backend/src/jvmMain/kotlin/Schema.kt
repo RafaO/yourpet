33d12ba0df9605ad1ef88c20b9ce4659590062f5
Rafa Ortega <rafao1989@hotmail.com>
2023-02-05 12:47:26 +0100

    [backend] add params to retrieve mocked pets or create them in the db

--- backend/src/jvmMain/kotlin/Schema.kt
+++ backend/src/jvmMain/kotlin/Schema.kt
@@ -1,13 +1,17 @@
 import com.apurebase.kgraphql.schema.dsl.SchemaBuilder
+import com.keller.yourpet.shared.mock.mockPetsList
 import com.keller.yourpet.shared.model.Filter
 import com.keller.yourpet.shared.model.Gender
 import com.keller.yourpet.shared.model.Pet
 
-fun SchemaBuilder.schemaValue(requestHandler: RequestHandler) {
+fun SchemaBuilder.schemaValue(requestHandler: RequestHandler, mockedPets: Boolean = false) {
     query("pets") {
         description = "Retrieve all pets"
         resolver { filter: Filter? ->
-            requestHandler.getPets(filter)
+            if (mockedPets)
+                mockPetsList()
+            else
+                requestHandler.getPets(filter)
         }
     }
 
