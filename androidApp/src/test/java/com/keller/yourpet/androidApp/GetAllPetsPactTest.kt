04b02641a2697f83eac8a940b1c1d8f9bf98d64d
RafaO <rafao1989@hotmail.com>
2023-11-25 17:46:33 +0100

    [refactor] use expression in pact builder

--- androidApp/src/test/java/com/keller/yourpet/androidApp/GetAllPetsPactTest.kt
+++ androidApp/src/test/java/com/keller/yourpet/androidApp/GetAllPetsPactTest.kt
@@ -8,7 +8,6 @@ import au.com.dius.pact.consumer.dsl.newArray
 import au.com.dius.pact.consumer.dsl.newJsonObject
 import au.com.dius.pact.consumer.dsl.newObject
 import au.com.dius.pact.consumer.junit.ConsumerPactTest
-import au.com.dius.pact.core.model.RequestResponsePact
 import com.apollographql.apollo3.ApolloClient
 import com.keller.yourpet.androidApp.utils.MainCoroutineRule
 import com.keller.yourpet.shared.api.PetsApiClient
@@ -33,56 +32,53 @@ class GetAllPetsPactTest : ConsumerPactTest() {
 
     override fun consumerName() = "YourPet Mobile client"
 
-    override fun createPact(builder: PactDslWithProvider): RequestResponsePact {
-
-        return builder
-            .uponReceiving("All pets query with all fields")
-            .path("/")
-            .method("POST")
-            .headers(mapOf("Content-Type" to "application/json"))
-            .body(
-                newJsonObject {
-                    stringValue("operationName", "GetAllPets")
-                    stringValue(
-                        "query",
-                        "query GetAllPets(\$filter: Filter!) { pets(filter: \$filter) { " +
-                                "id name imageUrl gender description } }"
-                    )
-                    newObject("variables") {
-                        newObject("filter") {
-                            newArray("genders") {
-                                stringValue("Male")
-                                stringValue("Female")
-                            }
+    override fun createPact(builder: PactDslWithProvider) = builder
+        .uponReceiving("All pets query with all fields")
+        .path("/")
+        .method("POST")
+        .headers(mapOf("Content-Type" to "application/json"))
+        .body(
+            newJsonObject {
+                stringValue("operationName", "GetAllPets")
+                stringValue(
+                    "query",
+                    "query GetAllPets(\$filter: Filter!) { pets(filter: \$filter) { " +
+                            "id name imageUrl gender description } }"
+                )
+                newObject("variables") {
+                    newObject("filter") {
+                        newArray("genders") {
+                            stringValue("Male")
+                            stringValue("Female")
                         }
                     }
                 }
-            )
-            .willRespondWith()
-            .status(200)
-            .body(
-                newJsonObject {
-                    newObject("data") {
-                        newArray("pets") {
-                            newObject {
-                                stringValue("id", "1")
-                                stringValue("name", "Fatality")
-                                stringValue("gender", "Female")
-                                stringValue("imageUrl", "https://picsum.photos/id/237/200/150")
-                                stringValue("description", "Fatality is a great dog")
-                            }
-                            newObject {
-                                stringValue("id", "2")
-                                stringValue("name", "Charlie")
-                                stringValue("gender", "Male")
-                                stringValue("imageUrl", "https://picsum.photos/id/1025/200/150")
-                                stringValue("description", "Charlie is a bit naughty")
-                            }
+            }
+        )
+        .willRespondWith()
+        .status(200)
+        .body(
+            newJsonObject {
+                newObject("data") {
+                    newArray("pets") {
+                        newObject {
+                            stringValue("id", "1")
+                            stringValue("name", "Fatality")
+                            stringValue("gender", "Female")
+                            stringValue("imageUrl", "https://picsum.photos/id/237/200/150")
+                            stringValue("description", "Fatality is a great dog")
+                        }
+                        newObject {
+                            stringValue("id", "2")
+                            stringValue("name", "Charlie")
+                            stringValue("gender", "Male")
+                            stringValue("imageUrl", "https://picsum.photos/id/1025/200/150")
+                            stringValue("description", "Charlie is a bit naughty")
                         }
                     }
                 }
-            ).toPact()
-    }
+            }
+        ).toPact()
 
     override fun runTest(mockServer: MockServer, context: PactTestExecutionContext) = runBlocking {
         val response = PetsApiClient(ApolloClient(mockServer.getUrl())).getPets(Filter.everything())
